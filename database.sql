41decbf55d7edbc3d3487d84e94f485c2b0d5e0e
Uliana <xxzz@ukr.net>
2018-03-02 04:41:31 +0200

    нова база даних

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
 -- Хост:                         127.0.0.1
--- Версия сервера:               5.6.38-log - MySQL Community Server (GPL)
--- Операционная система:         Win32
+-- Версия сервера:               5.7.20-log - MySQL Community Server (GPL)
+-- Операционная система:         Win64
 -- HeidiSQL Версия:              9.5.0.5196
 -- --------------------------------------------------------
 
@@ -21,11 +21,33 @@ CREATE TABLE IF NOT EXISTS `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product` varchar(255) NOT NULL,
   `quantity` int(11) NOT NULL DEFAULT '0',
-  `warehouse` varchar(255) NOT NULL,
+  `warehouse_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы test_work.products: ~3 rows (приблизительно)
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+REPLACE INTO `products` (`id`, `product`, `quantity`, `warehouse_id`) VALUES
+	(4, 'Product 1', 10, 1),
+	(5, 'Product 1', 5, 2),
+	(6, 'Product 1', 0, 3);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+
+-- Дамп структуры для таблица test_work.warehouses
+CREATE TABLE IF NOT EXISTS `warehouses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `warehouse` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы test_work.warehouses: ~3 rows (приблизительно)
+/*!40000 ALTER TABLE `warehouses` DISABLE KEYS */;
+REPLACE INTO `warehouses` (`id`, `warehouse`) VALUES
+	(1, 'WH1'),
+	(2, 'WH2'),
+	(3, 'WH3');
+/*!40000 ALTER TABLE `warehouses` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
